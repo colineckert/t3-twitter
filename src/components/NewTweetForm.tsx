29059c684d53e49742060d974815ea5ff4123fd7
colineckert <colineckert10@gmail.com>
2023-07-03 10:38:59 -0400

    build profile image and use discord cdn for src

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,10 +1,15 @@
+import { ProfileImage } from './ProfileImage';
 import { Button } from './Button';
+import { useSession } from 'next-auth/react';
 
 export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== 'authenticated') return;
+
   return (
     <form action="submit" className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        {/* <ProfileImage src='#url' /> */}
+        <ProfileImage src={session.data.user.image} />
         <textarea
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
