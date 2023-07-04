61b4040b7814544f392acdbf89f3ccecdf681827
colineckert <colineckert10@gmail.com>
2023-07-04 14:59:54 -0400

    build basic TweetCard

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { ProfileImage } from './ProfileImage';
 
 type Tweet = {
   id: string;
@@ -42,9 +44,45 @@ export function InfiniteTweetList({
         loader={'loading...'}
       >
         {tweets.map((tweet) => {
-          return <div key={tweet.id}>{tweet.content}</div>;
+          return <TweetCard key={tweet.id} {...tweet} />;
         })}
       </InfiniteScroll>
     </ul>
+  );
+}
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: 'short',
+});
+
+function TweetCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Tweet) {
+  return (
+    <li className="flex gap-4 border-b px-4 py-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="font-bold outline-none hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+      </div>
+    </li>
   );
 }
