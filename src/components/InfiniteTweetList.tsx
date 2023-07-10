c15e07fe6be19b1173cda2eca4cf8f5dc0b9175b
colineckert <colineckert10@gmail.com>
2023-07-09 17:51:22 -0700

    create and use loading spinner

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react';
 import { VscHeart, VscHeartFilled } from 'react-icons/vsc';
 import { IconHoverEffect } from './IconHoverEffect';
 import { api } from '~/utils/api';
+import { LoadingSpinner } from './LoadingSpinner';
 
 type Tweet = {
   id: string;
@@ -30,7 +31,7 @@ export function InfiniteTweetList({
   fetchNewTweets,
   hasMore = false,
 }: InfiniteTweetListProps) {
-  if (isLoading) return <h1>Loading...</h1>;
+  if (isLoading) return <LoadingSpinner />;
   if (isError) return <h1>Error...</h1>;
 
   if (tweets == null || tweets?.length === 0) {
@@ -45,7 +46,7 @@ export function InfiniteTweetList({
         dataLength={tweets.length}
         next={fetchNewTweets}
         hasMore={hasMore}
-        loader={'loading...'}
+        loader={<LoadingSpinner />}
       >
         {tweets.map((tweet) => {
           return <TweetCard key={tweet.id} {...tweet} />;
