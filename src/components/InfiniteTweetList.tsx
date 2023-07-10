e6e87ec03edebcd086e3c6fb8c353648a6ba36fd
colineckert <colineckert10@gmail.com>
2023-07-09 17:18:27 -0700

    set default hasMore to false

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -28,7 +28,7 @@ export function InfiniteTweetList({
   isError,
   isLoading,
   fetchNewTweets,
-  hasMore,
+  hasMore = false,
 }: InfiniteTweetListProps) {
   if (isLoading) return <h1>Loading...</h1>;
   if (isError) return <h1>Error...</h1>;
